b95f14e559b76b43392aef31e1ed223b6b8e0f2b
Murugan <chendur.murugan@iqvia.com>
2021-08-05 22:22:29 +0530

    first commit

---
+++ day3/myjs.js
@@ -0,0 +1,8 @@
+console.log('My script');
+document.getElementById("hid").innerHTML="Welcome to java script";
+document.getElementById("hid").style.color='red';
+ 
+functionchangeMe(){​​​​​​​​
+document.getElementById("hid").style.backgroundColor='green';
+}​​​​​​​​
+
